fd2eaf85fe2df4bd412e3b02a2e3cfcfd5c43a97
Eric Dong <ericdong66@gmail.com>
2017-03-18 11:43:17 -0700

    a better format of celery runner

--- Flansible/flansible/celery_runner.py
+++ Flansible/flansible/celery_runner.py
@@ -4,7 +4,6 @@ from subprocess import Popen, PIPE
 from flansible import api, app, celery, task_timeout
 
 
-
 @celery.task(bind=True, soft_time_limit=task_timeout, time_limit=(task_timeout+10))
 def do_long_running_task(self, cmd, type='Ansible'):
     with app.app_context():
@@ -21,28 +20,27 @@ def do_long_running_task(self, cmd, type='Ansible'):
         for line in iter(proc.stdout.readline, ''):
             print(str(line))
             output = output + line
-            self.update_state(state='PROGRESS', meta={'output': output,'description': "",'returncode': None})
+            self.update_state(state='PROGRESS', meta={'output': output, 'description': "", 'returncode': None})
 
         return_code = proc.poll()
         if return_code is 0:
-            meta = {'output': output, 
-                        'returncode': proc.returncode,
-                        'description': ""
+            meta = {'output': output,
+                    'returncode': proc.returncode,
+                    'description': ""
                     }
             self.update_state(state='FINISHED',
                               meta=meta)
         elif return_code is not 0:
-            #failure
-            meta = {'output': output, 
-                        'returncode': return_code,
-                        'description': str.format("Celery ran the task, but {0} reported error", type)
+            meta = {'output': output,
+                    'returncode': return_code,
+                    'description': str.format("Celery ran the task, but {0} reported error", type)
                     }
             self.update_state(state='FAILED',
-                          meta=meta)
+                              meta=meta)
         if len(output) is 0:
             output = "no output, maybe no matching hosts?"
-            meta = {'output': output, 
-                        'returncode': return_code,
-                        'description': str.format("Celery ran the task, but {0} reported error", type)
+            meta = {'output': output,
+                    'returncode': return_code,
+                    'description': str.format("Celery ran the task, but {0} reported error", type)
                     }
         return meta
